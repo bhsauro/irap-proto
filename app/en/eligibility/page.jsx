9e6cf83acc6170d56f1927f2d219e0df2bacd396
Brooke Sauro <bhsauro@gmail.com>
2024-04-07 22:53:56 -0400

    started working on eligibility page layout

--- app/en/eligibility/page.jsx
+++ app/en/eligibility/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Form from '../../../components/form-1'
 
 export default function Page({ navigation, user, userNavigation }) {
   return (
@@ -6,6 +7,7 @@ export default function Page({ navigation, user, userNavigation }) {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Eligibility</h1>
       </div>
+
     </header>
   );
 }
