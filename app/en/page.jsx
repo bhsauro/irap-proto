a88efbbf5fd12d528d6e4b7b6d107ba264a53236
Brooke Sauro <bhsauro@gmail.com>
2024-04-09 22:17:42 -0400

    saving changes

--- app/en/page.jsx
+++ app/en/page.jsx
@@ -9,6 +9,7 @@ return (
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Home</h1>
       </div>
+      
     </header>
 
 )};
