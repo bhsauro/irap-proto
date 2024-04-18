6a9e983fce75f174c39c42db34e4f5abd5330d20
Brooke Sauro <bhsauro@gmail.com>
2024-04-18 00:06:33 -0400

    shortened text

--- components/en-form-p1.jsx
+++ components/en-form-p1.jsx
@@ -8,7 +8,7 @@ export default function Page1() {
             
         Introduction - purpose of tool | posuere sed nibh id, porttitor rhoncus orci. Etiam sed condimentum purus. Quisque at eros ac tellus imperdiet facilisis consequat condimentum magna. Nam euismod accumsan turpis eu pharetra. <br></br><br></br>
         Aliquam at mi nec eros maximus porttitor. Praesent dapibus cursus sapien sed ornare. Nam vulputate purus at accumsan tristique. Fusce pulvinar condimentum eleifend. Curabitur in diam aliquam, imperdiet sapien non, pretium enim. Vivamus vitae ipsum bibendum, tincidunt magna et, lobortis tellus.
-        Nam id suscipit massa. Sed aliquam pharetra vulputate. Praesent vitae auctor purus, sit amet convallis quam. In quis ante a dui mattis commodo. Nunc egestas velit ullamcorper orci molestie, et laoreet sem dignissim. Ut dapibus pulvinar aliquam. Donec id erat felis. Proin sit amet egestas massa, non varius lacus. Aliquam elementum arcu dolor, et blandit turpis scelerisque quis. Nunc ultricies erat eu dignissim maximus. Nam ac hendrerit libero. In hac habitasse platea dictumst. Sed vel varius tellus. Phasellus sodales lacus quis magna commodo, sit amet laoreet eros tincidunt. Fusce quis ex at purus eleifend tempus. Etiam vestibulum sapien massa, et ornare purus tincidunt quis.
+        Nam id suscipit massa. Sed aliquam pharetra vulputate. Praesent vitae auctor purus, sit amet convallis quam. In quis ante a dui mattis commodo. 
 
         </p>
       
